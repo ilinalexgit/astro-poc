2c52575766d5a5715fae4b03238f3ac7ec11317d
Alex Ilin <alex.ilin@ymeadows.com>
2025-01-17 11:23:55 +0100

    add hero component, sanity integration, blog page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,15 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sanity(
+    {
+        projectId: "van4sbb6",
+        dataset: "production",
+        useCdn: false, // for static builds
+      }
+  )]
 });
